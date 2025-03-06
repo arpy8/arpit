c61a71505e96eb4c6973317aabc1bf0d34fc6180
Arpit <arpitsengar99@gmail.com>
2025-03-07 04:04:23 +0530

    Create main.py

---
+++ web/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+with open("./web/.ps1", "r") as f:
+    SHELL_SCRIPT_WIN = f.read()
+with open("./web/.sh", "r") as f:
+    SHELL_SCRIPT_UNIX = f.read()
+    
+@app.route("/", methods=["GET"])
+def index():
+    return Response("""Hi, I'm arpit""") 
+
+@app.route("/win", methods=["GET"])
+def windows(id):
+    return Response(SHELL_SCRIPT_WIN, mimetype="text/plain")
+
+@app.route("/unix", methods=["GET"])
+def windows(id):
+    return Response(SHELL_SCRIPT_UNIX, mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run()
